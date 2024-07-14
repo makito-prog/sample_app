57529917fe6e21dc374084393e29919526fc6800
makito-prog <147535008+makito-prog@users.noreply.github.com>
2024-07-14 12:05:47 +0000

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_13_134732) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_14_082416) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_13_134732) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
